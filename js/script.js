c1cb9d6ea1d6d44351f4283265e6e622c9035248
Sabin Subedi <sabinsubedi125@gmail.com>
2020-09-04 19:47:33 +0545

    Fixed bugs

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function changeLogo() {
     image.href.baseVal = "./Images/Image 1.svg";
   }
 }
+
+const circle_image = document.querySelectorAll(".circle-image");
+const circle_element = document.querySelectorAll(".circle__elements");
+
+for (let i = 0; i < circle_image.length; i++) {
+  circle_image[i].addEventListener("mouseover", function () {
+    console.log(circle_element[i].classList);
+    circle_element[i].classList.add("hover");
+  });
+  circle_image[i].addEventListener("mouseleave", function () {
+    console.log(circle_element[i]);
+    circle_element[i].classList.remove("hover");
+  });
+}
